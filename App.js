6693806ae35049f091cdf95fb1cf54c19e134bf7
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-18 18:44:33 -0300

    combustivel

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+    const [gas, setgas] = useState();
+    const [alc, setalc] = useState();
+    const [resultado, setResultado] = useState(0);
+
+    function dividir(){
+      setResultado(parseFloat(alc) / parseFloat(gas));
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+     
+     <View style={styles.nav}>
+      <Text style={styles.navtxt}>Cálculo do Combustível</Text>
+      </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <View style={styles.bloco}>
+      <Text style={styles.textoB}>Preço do Álcool</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType= "numeric"
+          value={alc}
+          onChangeText={(texto)=>setalc(texto)}
+        />
+      </View>
+      <View style={styles.bloco}>
+      <Text style={styles.textoB}>Preço da Gasolina</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType= "numeric"
+          value={gas}
+          onChangeText={(texto)=>setgas(texto)}
+        />
+      </View>
+
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+         style={styles.botao}
+          onPress = {dividir}>
+
+          <Text style={styles.textoBotao}>Calcular</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.bloco}>
+        <Text style={styles.textoB}>Resultado:{resultado}</Text>
+      </View>
+    </View>
+   );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FDDDDE',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    
+    navtxt: {
+      fontSize: 20,
+      display: 'flex', 
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    texto: {
+      color: '#000',
+      fontSize:30,
+    },
+
+    textoB: {
+      fontSize: 20,
+      marginTop: '10px',
+      display: 'flex', 
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    input:{
+      bordercolor: '#000',
+      borderWidth: 2,
+      fontSize: 30,
+      width: '80%',
+  
+    },
+
+    bloco: {
+      width: '100%',
+      alignItems: 'center',
+      marginTop: 30,
+      display: 'flex', 
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    botao: {
+      backgroundColor: '#000',
+      width: '80%',
+      textAlign: 'center',
+      marginTop: '12px'
+    },
+
+    textoBotao: {
+      color: '#fff',
+      fontSize:30,
+      textAlign: 'center',
+      alignItems: 'center'
+    }
+  });
